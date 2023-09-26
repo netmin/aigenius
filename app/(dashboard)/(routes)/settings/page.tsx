b5651f33db50ed2bf8ba698917b90338ad00c649
Vital D <netmin@pm.me>
2023-09-26 20:22:56 +0500

    stash subscription functions

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -1,26 +1,22 @@
 import { Settings } from "lucide-react";
+import Heading from "@/components/heading";
 
-import { Heading } from "@/components/heading";
-import { SubscriptionButton } from "@/components/subscription-button";
-import { checkSubscription } from "@/lib/subscription";
 
 const SettingsPage = async () => {
-  const isPro = await checkSubscription();
 
   return (
     <div>
       <Heading
-        title="Settings"
-        description="Manage account settings."
+        title="Настройки"
+        description="Управление настройками аккаунта"
         icon={Settings}
         iconColor="text-gray-700"
         bgColor="bg-gray-700/10"
       />
       <div className="px-4 lg:px-8 space-y-4">
         <div className="text-muted-foreground text-sm">
-          {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
+          Ваш текуший план бесплатный.
         </div>
-        <SubscriptionButton isPro={isPro} />
       </div>
     </div>
    );
